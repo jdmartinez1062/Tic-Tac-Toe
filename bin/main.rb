45e3566e1cf4efc8118ca46d2c5bbfe54caf01f6
Jonathan <48403308+mesielepush@users.noreply.github.com>
2019-11-01 11:14:29 -0600

    let players functions, oh i'm sorry, methods ready

--- bin/main.rb
+++ bin/main.rb
@@ -4,122 +4,8 @@
 require_relative '../lib/board.rb'
 require_relative '../lib/player.rb'
 require_relative '../lib/game.rb'
-
-
-
-def playergame_1(game)
-  putter = lambda {|x| puts x}
-  getter = lambda {gets.chomp}
-  game.board.print_board(putter)
-  while game.gaming
-
-    if game.turn == :p1
-
-      puts ''
-      puts "| It's Your turn Human, tell me your move |"
-      puts "|     Or enter: 'end' for leaving         |"
-      puts ''
-      move = gets.chomp
-      if move == 'end'
-        game.gaming = false
-      elsif game.board.key.include? move.to_sym
-        unless game.board.key[move.to_sym] == '-'
-          puts '|       That position is taken        |'
-          next
-        end
-        game.board.key[move.to_sym] = 'x'
-        game.turn = :p2
-        system('cls')
-        puts "| You just played #{move}"
-        game.board.print_board(putter)
-        puts '|           PRESS ANY KEY             |'
-        gets.chomp
-
-      else
-        puts "| I don't know what you're saying, repeat please"
-      end
-
-    elsif game.turn == :p2
-
-      getting_computer_move = true
-      while getting_computer_move
-        move = game.board.key.keys.sample
-        next unless game.board.key[move.to_sym] == '-'
-
-        game.board.key[move.to_sym] = '0'
-        system('cls')
-        puts ''
-        puts "| It's MY turn Human, prepare for this: |"
-        puts "| I just played #{move}"
-        puts ''
-        game.board.print_board(putter)
-        puts '|           PRESS ANY KEY             |'
-        gets.chomp
-        game.turn = :p1
-        getting_computer_move = false
-      end
-    end
-  end
-end
-
-def playergame_2(game)
-  game.board.print_board
-  while game.gaming
-    if game.turn == :p1
-
-      puts ''
-      puts "| It's Your turn player 1, tell me your move |"
-      puts "|     Or enter: 'end' to surrender        |"
-      puts ''
-      move = gets.chomp
-      if move == 'end'
-        game.gaming = false
-      elsif game.board.key.include?(move.to_sym)
-        unless game.board.key[move.to_sym] == '-'
-          puts '|       That position is taken        |'
-          next
-        end
-        game.board.key[move.to_sym] = 'x'
-        game.turn = :p2
-        system('cls')
-        puts "| You just played #{move}"
-        game.board.print_board
-        puts '|           PRESS ANY KEY             |'
-        gets.chomp
-
-      else
-        puts "| I don't know what you're saying, repeat please"
-      end
-
-    elsif game.turn == :p2
-
-      puts ''
-      puts "| It's Your turn player 2, tell me your move |"
-      puts "|     Or enter: 'end' to surrender      |"
-      puts ''
-      move = gets.chomp
-      if move == 'end'
-        game.gaming = false
-      elsif game.board.key.include?(move.to_sym)
-        unless game.board.key[move.to_sym] == '-'
-          puts '|       That position is taken        |'
-          next
-        end
-        game.board.key[move.to_sym] = '0'
-        game.turn = :p1
-        system('cls')
-        puts "| You just played #{move}"
-        game.board.print_board
-        puts '|           PRESS ANY KEY             |'
-        gets.chomp
-
-      else
-        puts "| I don't know what you're saying, repeat please"
-      end
-    end
-  end
-end
-
+putter = lambda {|x| puts x}
+getter = lambda {gets.chomp}
 system('cls')
 puts '| ################################## |'
 puts '        Welcome to Tic-Tac-Toe'
@@ -133,7 +19,6 @@ puts ''
 getting_players = true
 getting_difficulty = true
 while getting_players
-
   n_players = gets.chomp
 
   case n_players
@@ -169,7 +54,7 @@ end
 
 case n_players
 when '1'
-  playergame_1(game)
+  game.playergame_1(putter,getter)
 when '2'
-  playergame_2(game)
+  game.playergame_2(putter,getter)
 end
--- lib/game.rb
+++ lib/game.rb
@@ -20,4 +20,114 @@ class Game
     end
     @gaming = true
   end
+
+  def playergame_1(outputter,inputer)
+    
+    self.board.print_board(outputter)
+    while self.gaming
+      if self.turn == :p1
+  
+        ['', "| It's Your turn Human, tell me your move |",
+        "|     Or enter: 'end' for leaving         |", ''].each{|x| outputter.call(x)}
+        move = inputer.call
+        if move == 'end'
+          self.gaming = false
+        elsif self.board.key.include? move.to_sym
+          unless self.board.key[move.to_sym] == '-'
+            outputter.call('|       That position is taken        |')
+            next
+          end
+          self.board.key[move.to_sym] = 'x'
+          self.turn = :p2
+          system('cls')
+          outputter.call("| You just played #{move}")
+          self.board.print_board(outputter)
+          outputter.call('|           PRESS ANY ENTER             |')
+          inputer.call
+  
+        else
+          outputter.call("| I don't know what you're saying, repeat please")
+        end
+  
+      elsif self.turn == :p2
+  
+        getting_computer_move = true
+        while getting_computer_move
+          move = self.board.key.keys.sample
+          next unless self.board.key[move.to_sym] == '-'
+  
+          self.board.key[move.to_sym] = '0'
+          system('cls')
+          ['',
+          "| It's MY turn Human, prepare for this: |",
+          "| I just played #{move}",
+          ''].each{|x| outputter.call(x)}
+
+          self.board.print_board(outputter)
+          outputter.call('|           PRESS ANY ENTER             |')
+          inputer.call
+          self.turn = :p1
+          getting_computer_move = false
+        end
+      end
+    end
+  end
+
+  def playergame_2(outputter,inputer)
+    self.board.print_board(outputter)
+    while self.gaming
+      if self.turn == :p1
+  
+        ['', "| It's Your turn Player One, tell me your move |",
+        "|     Or enter: 'end' for leaving         |", ''].each{|x| outputter.call(x)}
+        move = inputer.call
+        if move == 'end'
+          self.gaming = false
+        elsif self.board.key.include? move.to_sym
+          unless self.board.key[move.to_sym] == '-'
+            outputter.call('|       That position is taken        |')
+            next
+          end
+          self.board.key[move.to_sym] = 'x'
+          self.turn = :p2
+          system('cls')
+          outputter.call("| You just played #{move}")
+          self.board.print_board(outputter)
+          outputter.call('|           PRESS ANY ENTER             |')
+          inputer.call
+  
+        else
+          outputter.call("| I don't know what you're saying, repeat please")
+        end
+  
+      elsif self.turn == :p2
+  
+        ['',
+        "| It's Your turn player 2, tell me your move |",
+        "|     Or enter: 'end' to surrender      |",
+        ''].each { |x| outputter.call(x)}
+        move = inputer.call
+        if move == 'end'
+          self.gaming = false
+        elsif self.board.key.include?(move.to_sym)
+          unless self.board.key[move.to_sym] == '-'
+            outputter.call('|       That position is taken        |')
+            next
+          end
+          self.board.key[move.to_sym] = '0'
+          self.turn = :p1
+          system('cls')
+          outputter.call("| You just played #{move}")
+          self.board.print_board(outputter)
+          outputter.call('|           PRESS ANY ENTER             |')
+          inputer.call
+  
+        else
+          outputter.call("| I don't know what you're saying, repeat please")
+        end
+      end
+    end
+  end
+  
+
 end
