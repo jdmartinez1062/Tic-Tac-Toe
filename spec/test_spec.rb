0014dfc5fd84655faeb522636e8c7a729379d349
mesielepush <48403308+mesielepush@users.noreply.github.com>
2019-11-05 16:39:09 -0600

    checking winning combos

---
+++ spec/test_spec.rb
@@ -0,0 +1,19 @@
+require './lib/board.rb'
+require './lib/game.rb'
+require './lib/player.rb'
+
+describe 'Tic-tac-toe' do
+    let(:board) {Board.new()}
+    let(:play)  {board.key[:a1] = '0'}
+    let(:play1) {board.key[:a2] = '0'}
+    let(:play2) {board.key[:a3] = '0'}
+    
+    describe '#somebody_win?' do
+        it 'tells you if a combo is a winning combo' do
+            play
+            play1
+            play2
+            expect(board.somebody_win?).to eql('0')
+        end
+    end
+end
